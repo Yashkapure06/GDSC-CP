b0b70c050a6f71210ef889d1a6069c903b683ffe
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-18 10:19:18 +0530

    Arithmetic Operators

---
+++ src/Day1/ArithmeticOperations.java
@@ -0,0 +1,20 @@
+package Day1;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		// Write Simple Program using Arithmetic operators
+		
+		int i = 10;	
+		System.out.println(i); // 10
+		int i2 = ++i;	
+		System.out.println(i2); //11
+		System.out.println("---");
+		System.out.println(i+i2);
+		System.out.println(i-i2);
+		System.out.println(i*i2);
+		System.out.println(i/i2);
+		System.out.println(i%i2);
+	}
+
+}
