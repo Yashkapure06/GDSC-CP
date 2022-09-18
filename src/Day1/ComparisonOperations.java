fa0a79089acbcd407c5a48155dbc89fc1cc80d70
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-18 10:23:23 +0530

    Comparison Operations

---
+++ src/Day1/ComparisonOperations.java
@@ -0,0 +1,19 @@
+package Day1;
+
+public class ComparisonOperations {
+
+	public static void main(String[] args) {
+		//  Write a Simple Program using Comparison Operators.
+		int i1 = 10;
+		int i2 = 20;
+		
+		System.out.println(i1 == i2);	// false
+		System.out.println(i1 != i2);	// true
+		System.out.println(i1 > i2);	// false
+		System.out.println(i1 < i2);	// true
+		System.out.println(i1 >= i2);	// false
+		System.out.println(i1 <= i2);	// true
+		
+	}
+
+}
