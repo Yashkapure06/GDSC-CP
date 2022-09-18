d46e780e952f3db02f546ecf0ab06aa0c82a8750
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-18 10:45:56 +0530

    update arithmetic opration

---
+++ src/Day1_18_09/ArithmeticOperations.java
@@ -0,0 +1,19 @@
+package Day1_18_09;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		// Write Simple Program using Arithmetic operators
+		
+		int i = 10;	
+		int i2 = 15;
+		
+		System.out.println("---");
+		System.out.println(i+i2);// 25
+		System.out.println(i-i2);// -5
+		System.out.println(i*i2);// 150
+		System.out.println(i/i2);// 0
+		System.out.println(i%i2);// 10
+	}
+
+}
--- src/Day1_18_09/ComparisonOperations.java
+++ src/Day1_18_09/ComparisonOperations.java
@@ -1,4 +1,4 @@
-package Day1;
+package Day1_18_09;
 
 public class ComparisonOperations {
 
