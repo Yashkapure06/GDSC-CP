4b82384ec17027b439ef9dbe451b4bf35b9ee361
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-19 10:04:18 +0530

    added javasccript files

---
+++ src/ArithmeticOperation.js
@@ -0,0 +1,24 @@
+
+const a = 5;
+const b = 15;
+//function to add two numbers    
+function add(a, b) {
+    return a + b;
+}
+//function to subtract two numbers    
+function subtract(a, b) {
+    return a - b;
+}
+//function to multiply two numbers    
+function multiply(a, b) {
+    return a * b;
+}
+//function to divide two numbers    
+function divide(a, b) {
+    return a / b;
+}
+//function to find the remainder of two numbers    
+function remainder(a, b) {
+    return a % b;
+}
+
