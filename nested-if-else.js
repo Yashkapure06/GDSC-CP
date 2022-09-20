fbfbc2b6d84ef3f2c0dc989df1e10fd795473ae9
Yashkapure06 <61585443+Yashkapure06@users.noreply.github.com>
2022-09-20 09:26:58 +0530

    Q5. Simple nested if else statement

---
+++ nested-if-else.js
@@ -0,0 +1,18 @@
+//Q5. 5) Write A Program using nested if else statement
+//Date: 20-09-2022
+let a = 5;
+let b = 6;
+let c = 7;
+if (a > b) {
+  if (a > c) {
+    console.log("A is greater");
+  } else {
+    console.log("C is greater");
+  }
+} else {
+  if (b > c) {
+    console.log("B is greater");
+  } else {
+    console.log("C is greater");
+  }
+}
